3943aae03cbd9c95bffce10e459778747520c7a6
奥田竜矢 <okuda.ryuya0765@gmail.com>
2020-02-28 00:56:31 +0900

    カスタムセルの設定

--- shareHouse/Home/NameCell.swift
+++ shareHouse/Home/NameCell.swift
@@ -9,6 +9,11 @@
 import Foundation
 import UIKit
 
-final class CustomCell: UITableViewCell {
-    
+final class NameCell: UITableViewCell, CellReusable {
+    @IBOutlet weak var nameLavel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
 }
--- shareHouse/Protocols/Reusable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  shareHouse
-//
-//  Created by 奥田竜矢 on 2020/02/26.
-//  Copyright © 2020 okuda ryuya. All rights reserved.
-//
-
-import Foundation
