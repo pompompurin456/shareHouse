20dc51b536a20f403bb16daabb059e69b1d33662
奥田竜矢 <okuda.ryuya0765@gmail.com>
2020-02-28 15:31:08 +0900

    カスタムセルの実装

--- shareHouse/Home/ViewController.swift
+++ shareHouse/Home/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 import Foundation
 
 final class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+
+    let sectionTitle = ["お風呂", "洗濯機"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Home"
         setupNavigationController()
+        setUpTableView()
     }
     
     @objc private func presentAddView() {
@@ -27,5 +32,46 @@ final class ViewController: UIViewController {
     private func setupNavigationController() {
         let rigthButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.presentAddView))
         navigationItem.rightBarButtonItem = rigthButton
+    }
+
+    private func setUpTableView() {
+        mainTableView.dataSource = self
+        mainTableView.register(cellType: NameCell.self)
+        mainTableView.tableFooterView = UIView()
+    }
+}
+
+//extension ViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 44.0
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
+//
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 44.0
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as! MyHeaderView
+//        header.apply(text: sectionTitle[section])
+//        return header
+//    }
+//}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(with: NameCell.self, for: indexPath)
+        return cell
     }
 }
